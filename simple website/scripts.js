a3e106af17ec280c4a429f2de82c7d04d4e29c09
Priya Kushwaha <99608708+Kushpriya@users.noreply.github.com>
2024-07-20 19:28:07 +0545

    simple website

---
+++ simple website/scripts.js
@@ -0,0 +1,33 @@
+function login() {
+  var a = document.getElementById("name").value;
+  var b = document.getElementById("address").value;
+  var c = document.getElementById("email").value;
+  var d = document.getElementById("phone").value;
+  var e = document.getElementById("feedback").value;
+
+  if (a == "" || a == null) 
+    {
+    alert("Please enter your name.");
+    return false;
+  } 
+  else if (b === "" || b === null) {
+    alert("Please enter your address.");
+    return false;
+  } 
+  else if (c === "" || c === null) {
+    alert("Please enter your email.");
+    return false;
+  }
+   else if (d === "" || d.length < 10) {
+    alert("Please enter a number that must be 10 characters long.");
+    return false;
+  }
+   else if (e == "" || e===null) {
+    alert("Please enter any feedback.");
+    return false;
+  } 
+  else {
+    alert("Form submitted");
+    return true;
+  }
+}
