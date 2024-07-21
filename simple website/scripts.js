9fbf915d1a99817ac4816266afe07e90e76e1f1e
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-07-21 22:26:39 +0545

    updated

--- simple website/scripts.js
+++ simple website/scripts.js
@@ -4,21 +4,21 @@ let navlinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
   sections.forEach(sec => {
     let top = window.scrollY;
-    let offset = sec.offsetTop -500;
+    let offset = sec.offsetTop - 200;
     let height = sec.offsetHeight;
-    let id =sec.getAttribute('id');
+    let id = sec.getAttribute('id');
 
-    if(top >= offset && top < offset + height) {
-      navlinks.forEach(links =>{
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(links => {
         links.classList.remove("active");
-        document.querySelector('header nav a[href*=' + id + ']').classlist.add('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
       });
-    };
+    }
   });
 };
 
 
-function login() {
+function submit() {
   var a = document.getElementById("name").value;
   var b = document.getElementById("address").value;
   var c = document.getElementById("email").value;
