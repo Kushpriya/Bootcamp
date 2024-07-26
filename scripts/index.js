cdbe137051193ddc8ec59071d7fc542aaf1280a4
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-07-26 10:35:47 +0545

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ function login() {
   var email = document.getElementById("email").value.trim();
   var phone = document.getElementById("phone").value.trim();
 
-  var regx_name = /^[A-Z a-z.]$/;
+  var regx_name = /^([a-zA-Z]{3,30}\s*)+$/;
   var regx_address = /^[A-Z a-z0-9\s,.'-]{3,}$/;
   var regx_email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   var regx_phone = /^[789][0-9]{9}$/;
@@ -59,37 +59,6 @@ function login() {
       "Phone number is invalid";
     return false;
   }
-
+  alert("Form Submitted");
   return true;
 }
-
-// function checkLoginStatus() {
-//   const loggedIn = localStorage.getItem("loggedIn") === "true";
-//   const loginLink = document.getElementById("loginlink");
-//   const signUpLink = document.getElementById("signuplink");
-//   const logoutLink = document.getElementById("logoutlink");
-
-//   if (loggedIn) {
-//     loginLink.style.display = "none";
-//     signUpLink.style.display = "none";
-//     logoutLink.style.display = "inline";
-//   } else {
-//     loginLink.style.display = "inline";
-//     signUpLink.style.display = "inline";
-//     logoutLink.style.display = "none";
-//   }
-// }
-
-// function handleLogout() {
-//   localStorage.removeItem("loggedIn");
-//   checkLoginStatus();
-// }
-
-// document
-//   .getElementById("logoutlink")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
-//     handleLogout();
-//   });
-
-// document.addEventListener("DOMContentLoaded", checkLoginStatus);
